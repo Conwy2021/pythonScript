32a56c530b6a81a1917cac8a17789a0248094322
Conwy2021 <FQ06FQ06@gmail.com>
2022-11-23 21:59:35 +0800

    修改移动 点击 分开

--- test4.py
+++ test4.py
@@ -5,11 +5,21 @@ import pyautogui
 try:
     while True:
         time.sleep(2)
-        pyautogui.click(896, 586, duration=1)  # interval 点击间隔
-        pyautogui.click(889, 491, duration=1)
-        pyautogui.click(694, 390, duration=1)
-        pyautogui.click(712, 749, duration=1)
-        pyautogui.click(888, 353, duration=1)
+        # pyautogui.click(896, 586, duration=1)  # interval 点击间隔
+        # pyautogui.click(889, 491, duration=1)
+        # pyautogui.click(694, 390, duration=1)
+        # pyautogui.click(712, 749, duration=1)
+        # pyautogui.click(888, 353, duration=1)
+        pyautogui.moveTo(896, 586, duration=1)
+        pyautogui.click()
+        pyautogui.moveTo(889, 491, duration=1)
+        pyautogui.click()
+        pyautogui.moveTo(694, 390, duration=1)
+        pyautogui.click()
+        pyautogui.moveTo(712, 749, duration=1)
+        pyautogui.click()
+        pyautogui.moveTo(888, 353, duration=1)
+        pyautogui.click()
         time.sleep(7200)
 except KeyboardInterrupt:
     print('\nExit.')
